05d4a942443bcf84278e3a4a0cd6b00225d29c6a
Muthuswamy <2391791@cognizant.com>
2025-03-06 14:18:09 +0530

    Checking the OCR

--- src/services/EnvironmentConfigService.js
+++ src/services/EnvironmentConfigService.js
@@ -1,7 +1,7 @@
 // EnvironmentConfigService.js
 export const getOcrServiceData = () => {
   return {
-    url: "/process/comprehensive/gateway", // Use relative path
+    url: "https://docs.idresponse.com/process/comprehensive/gateway",
     user: "wsibm@rjrt.com",
     key: "Rfjguihf4!dhfuijvhu@df",
     service: "DCAMSPLUS5.0.TEST",
